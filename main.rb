171f1ce6d8c4836f11e2ab8dcc68a237d0eaab97
Cyprian Gepfert cgepfert@gmail.com <cgepfert@gmail.com>
2015-02-24 23:40:14 +0100

    add RESTful CRUD

--- main.rb
+++ main.rb
@@ -37,3 +37,39 @@ get '/instance' do
 	@name = "DAZ"
 	slim :show
 end
+
+get '/songs' do
+	@songs = Song.all
+	slim :songs
+end
+get '/songs/new' do
+	@song = Song.new
+	slim :new_song
+end
+
+get '/songs/:id' do
+	@song = Song.get(params[:id])
+	slim :show_song
+end
+
+post '/songs' do 
+	song = Song.create(params[:song])
+	redirect to ("/songs/#{song.id}")
+end
+
+get '/songs/:id/edit' do
+	@song = Song.get(params[:id])
+	slim :edit_song
+end
+
+put '/songs/:id' do
+	song = Song.get(params[:id])
+	song.update(params[:song])
+	redirect to("/songs/#{song.id}")
+end
+
+delete '/songs/:id' do
+	song = Song.get(params[:id])
+	song.destroy
+	redirect to("/songs")
+end
